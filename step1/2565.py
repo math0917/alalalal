4e731bb7bc11ad82bb59acbf7d501aa69b8e4a35
math0917 <math0917@naver.com>
2021-07-23 19:09:32 +0900

    good

--- step1/2565.py
+++ step1/2565.py
@@ -1,25 +1,38 @@
 import sys
 
+
 n = int(sys.stdin.readline())
 
 arr= [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 ptr = [j[0] for j in arr]
-N = max(ptr)
-result = []
-for i in range(N):
-    result.append(0)
-while(True):
-    for i in range(n):
-        count = 0
-        for j in range(n):
-            if arr[i][1] > arr[j][1] and arr[i][0] < arr[j][0]:
-                
-                count+=1
-            if arr[i][0] > arr[j][0] and arr[j][1]> arr[i][1]:
-                count+=1
 
-        result[arr[i][0]-1] = count
-    if len(set(result)) == 1:
-        break
+max_val = max(ptr)
+
+result = []
+
+for i in range(max_val+1):
+    result.append(501)
+
+
+for i in range(n):
+    index = arr[i][0]
+    val = arr[i][1]
+
+    result[index] = val
+
+final = [[0,1]]
+
+for i in range(1,max_val+1):
+    ptr=result[:i]
+    small_count = [j for j in range(len(ptr)) if ptr[j] < result[i]]
+ 
+    if len(small_count) == 0:
+        final.append([0,1])
     else:
-        max_idx = 
+        max_val = [final[j][1] for j in small_count]
+        max_val1 = max(max_val)
+        final.append([max_val1,max_val1+1])
+
+
+max_v = [max(final[j]) for j in range(len(final)) if result[j] != 501]
+print(n-max(max_v))
