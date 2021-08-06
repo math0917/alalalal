5f4276d9fed94b346ca614a3739617a090a4d94e
math0917 <math0917@naver.com>
2021-08-06 16:32:35 +0900

    god

--- step1/13305.py
+++ step1/13305.py
@@ -8,3 +8,21 @@ length = list(map(int,sys.stdin.readline().split()))
 
 cost = list(map(int,sys.stdin.readline().split()))
 
+result =[]
+
+result.append([(0,length[0]*cost[0])])
+
+for i in range(1,n-1):
+    result.append([])
+    for j in result[i-1]:
+        from_s = j[0]
+        cost_s = j[1]
+        if cost_s + cost[i] * length[i] < cost_s + cost[from_s] * length[i] :
+            result[i].append((i, cost_s+cost[i]*length[i]))
+        else:
+            result[i].append((from_s, cost_s+ cost[from_s]*length[i]))
+min_S = result[-1][0][1]
+for i in result[-1]:
+    if i[1] < min_S:
+        min_S = i[1]
+print(min_S)
--- step1/1932.py
+++ step1/1932.py
@@ -4,15 +4,14 @@ n = int(sys.stdin.readline())
 
 arr = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-result = []
-result.append([arr[0][0]])
-result.append([arr[0][0]+arr[1][0],arr[0][0]+arr[1][1]])
+result = [[arr[0][0]]]
 
-
-for i in range(2,n):
+for i in range(1,len(arr)):
     result.append([])
-    result[i].append(result[i-1][0]+arr[i][0])
-    for j in range(1,len(arr[i])-1):
-        result[i].append(max(result[i-1][j-1]+arr[i][j],result[i-1][j]+arr[i][j]))
-    result[i].append(result[i-1][-1]+arr[i][-1])
+    for j in range(i+1):
+        result[i].append(0)
+    result[i][0]=result[i-1][0]+arr[i][0]
+    result[i][-1] = result[i-1][-1] + arr[i][-1]
+    for j in range(1,len(result[i])-1):
+        result[i][j] = max(result[i-1][j-1]+arr[i][j], result[i-1][j]+arr[i][j])
 print(max(result[-1]))
