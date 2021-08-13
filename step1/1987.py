728da5dd949be71be58cf56087fc61744b0193c6
math0917 <math0917@naver.com>
2021-08-14 01:30:06 +0900

    good

--- step1/1987.py
+++ step1/1987.py
@@ -1,41 +1,53 @@
 import sys
 import collections 
-import copy
 
+def per(stack,arr,max):
+        this_turn = stack.pop()
+        this_row = this_turn[0][0]
+        this_col = this_turn[0][1]
+        
+        ptr = [[this_row-1, this_col], [this_row+1, this_col], [this_row, this_col-1], [this_row, this_col+1]]
+        count=0
+        for i in ptr:
+            
+            row = i[0]
+            col = i[1]
+            
+            if row >= n or row <= -1 or col >= m or col <= -1 :
+                continue
+            this_num = ord(arr[row][col])
+            if this_turn[1][this_num - 65] == False:
+                continue
+            else:
+                this_turn[1][this_num - 65] = False
+               
+                stack.append([(row,col),this_turn[1]])
+
+                max = per(stack,arr,max)
+
+                this_turn[1][this_num - 65] = True
+                count+=1
+            
+        if not count:
+            ptr = this_turn[1].count(False)
+            if max < ptr:
+                max = ptr
+        
+        return max
+        
 n, m = map(int,sys.stdin.readline().split())
 
 arr = [list(map(str,sys.stdin.readline().strip())) for _ in range(n)]
-bu = set()
 
-for i in range(len(arr)):
-    for j in range(len(arr[0])):
-        bu.add(arr[i][j])
+visit = set()
 
-stack = collections.deque([[(0,0), [bu-{arr[0][0]}]]])
+alpha = [True]*26
+alpha[ord(arr[0][0]) - ord('A')] = False
+stack = collections.deque([[(0,0), alpha]])
 
 
-max = 0
+result = per(stack,arr,0)
 
-while(len(stack)!=0):
-    this_turn = stack.pop()
-    this_row = this_turn[0][0]
-    this_col = this_turn[0][1]
+print(result)            
+
     
-    ptr = [[this_row-1, this_col], [this_row+1, this_col], [this_row, this_col-1], [this_row, this_col+1]]
-    count=0
-    for i in ptr:
-        row = i[0]
-        col = i[1]
-        if row >= n or row <= -1 or col >= m or col <= -1:
-            continue
-        if arr[row][col] in this_turn :
-            stack.append([(row,col), bu-(this_turn+{arr[row][col]})])
-            count+=1            
-        else:
-            continue
-           
-    if not count:
-        if max<len(this_turn[1]):
-            max = len(this_turn[1])
-    
-print(max)
