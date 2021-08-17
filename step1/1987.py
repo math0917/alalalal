1d776c128edb2174632e3a7329ef19ee7d2e467d
math0917 <math0917@naver.com>
2021-08-17 15:48:03 +0900

    GOOD

--- step1/1987.py
+++ step1/1987.py
@@ -1,29 +1,28 @@
 import sys
 import collections 
 
-def per(idx,arr,alpha,depth):
+def per(idx,depth):
         global result
         this_row = idx[0]
         this_col = idx[1]
-        dx = [-1,0,1,0]
-        dy = [0,1,0,-1]
+        ptr = [[this_row-1, this_col], [this_row+1, this_col], [this_row, this_col-1], [this_row, this_col+1]]
         count=0
-        for i in range(4):
+        for i in ptr:
             
-            row = this_row+dx[i]
-            col = this_col+dy[i]
+            row = i[0]
+            col = i[1]
             
             if row >= n or row <= -1 or col >= m or col <= -1 or alpha[arr[row][col]] == False:
                 continue
             else:
-                alpha[arr[row][col]] = False
+                alpha[arr[row][col]] = 0
                
                 idx[0] = row
                 idx[1] = col
 
-                per(idx,arr,alpha,depth+1)
+                per(idx,depth+1)
 
-                alpha[arr[row][col]] = True
+                alpha[arr[row][col]] = 1
                 count+=1
             
         if not count:
@@ -32,18 +31,15 @@ def per(idx,arr,alpha,depth):
                 result = depth
         
         
-
+        
 n, m = map(int,sys.stdin.readline().split())
 
-arr = [list(map(str,sys.stdin.readline().strip())) for _ in range(n)]
-alpha = dict()
-for i in range(n):
-    for j in range(m):
-        arr[i][j] = ord(arr[i][j])- 65
-        alpha[arr[i][j]] = True
-alpha[arr[0][0]] = False
+arr = [list(map(lambda x : ord(x) - 65,sys.stdin.readline().strip())) for _ in range(n)]
+
+alpha = [1]*26
+alpha[arr[0][0]] = 
 
 result = 1
 
-per([0,0],arr,alpha,1)
+per([0,0],1)
 print(result)
