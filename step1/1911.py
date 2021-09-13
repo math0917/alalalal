d98e1c7dca60f630e3a5c69440156d3f76fc5ad2
math0917 <math0917@naver.com>
2021-09-13 17:57:04 +0900

    good

--- step1/1911.py
+++ step1/1911.py
@@ -1,28 +1,25 @@
 import sys
-import heapq
+import collections
+n, l = map(int,sys.stdin.readline().split())
 
-def sol(n,stations,l,p):
-    stations.sort(reverse=True)
-    heap = []
-
-    count = 0
-
-    while p<l:
-        while stations and stations[-1][0] <= p:
-            dist, fuel = stations.pop()
-            heapq.heappush(heap, -fuel)
-        if not heap:
-            break
-        p -=heapq.heappop(heap)
-        count+=1
-    if l<=p:
-        return count
-    return -1
-
-n = int(sys.stdin.readline())
-
-stations = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
-
-goal, gas = map(int,sys.stdin.readline().split())
-
-print(sol(n,stations,goal,gas))
+water = collections.deque([])
+arr = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+arr.sort()
+count = 0
+left_count = 0
+left_num = 0
+for i in arr:
+    i[0] = max(i[0],left_num+left_count+1)
+    if i[0]>=i[1]:
+        continue
+    if (i[1] - i[0]) % l:
+        count += ((i[1]-i[0])//l)+ 1
+        left_num = i[1] - 1
+        left_count = l - (i[1]-i[0])%l
+    else:
+        count += ((i[1]-i[0])//l)
+        left_num = 0
+        left_count = 0
+   
+    
+print(count)
