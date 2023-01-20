57b7d85bbaf52c8526ab6e7eeb4406aa7e5bcd49
Kim Won Pyo <69501435+math0917@users.noreply.github.com>
2023-01-20 14:42:58 +0900

    [Silver I] Title: 등차수열과 쿼리, Time: 1172 ms, Memory: 318588 KB -BaekjoonHub

--- 백준/Silver/23888. 등차수열과 쿼리/등차수열과 쿼리.java
+++ 백준/Silver/23888. 등차수열과 쿼리/등차수열과 쿼리.java
@@ -1,53 +1,60 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int a, d, q;
+    static long a, d, q;
     static StringBuilder sb = new StringBuilder();
     static long[] arr;
-    static Queue<Integer> queue;
+    static Deque<Integer> queue;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        q = Integer.parseInt(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        d = Long.parseLong(st.nextToken());
+        q = Long.parseLong(br.readLine());
         for (int t = 0; t < q; t++) {
             st = new StringTokenizer(br.readLine());
             int procedure = Integer.parseInt(st.nextToken()),
                     from = Integer.parseInt(st.nextToken()),
                     to = Integer.parseInt(st.nextToken());
             if (procedure == 1) {
-                long first = a + (long) (from - 1) * d;
-                sb.append((long) (to - from + 1) * first + (long) (to - from) * (to - from + 1) * d / 2);
+//                9999991000000
+
+                sb.append((long)(to - from + 1) * a + ((long)(to + from - 2) * (to - from +1) / 2 * d));
+//                sb.append((new BigInteger(String.valueOf((to - from + 1)* first)).add
+//                        (new BigInteger(String.valueOf((to - from) * (to - from + 1))).multiply(new BigInteger(String.valueOf(d))).divide(new BigInteger("2")))));
                 sb.append("\n");
             } else {
-                queue = new ArrayDeque<>();
-                for (int i = from; i <= to; i++) {
-                    queue.add(a + (i-1) * d);
+                if (from != to) {
+                    sb.append(gcd(a, d));
+                    sb.append("\n");
+                } else {
+                    sb.append(a + (from- 1) *d);
+                    sb.append("\n");
                 }
-                while (!queue.isEmpty()) {
-                    if (queue.size() == 1) {
-                        sb.append(queue.poll());
-                        sb.append("\n");
-                    } else {
-                        queue.add(gcd(queue.poll(), queue.poll()));
-                    }
-                }
+
             }
         }
         System.out.println(sb.toString());
     }
 
-    private static int gcd(int a, int d) {
-        int min = Math.min(a, d);
-        int max = Math.max(a, d);
+    private static BigInteger multiply(long a1, long a2) {
+        return (new BigInteger(String.valueOf(a1)).multiply(new BigInteger(String.valueOf(a2))));
+    }
+
+    private static long gcd(long a, long d) {
+        long min = Math.min(a, d);
+        long max = Math.max(a, d);
+        if (min == 0) {
+            return max;
+        }
         if (max % min == 0) {
             return min;
         } else {
