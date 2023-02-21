4f6ae13048efe7527cd32f982de24fe32b87df8d
Kim Won Pyo <69501435+math0917@users.noreply.github.com>
2023-02-21 13:32:58 +0900

    [Gold II] Title: 빵집, Time: 344 ms, Memory: 41988 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.java
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -1,66 +1,52 @@
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
     static int r, c;
-    static char[][]  map;
+    static char[][] bread;
+    static boolean[][] visited;
+    static int result = 0;
     static int[] dx = {-1, 0, 1};
     static int[] dy = {1, 1, 1};
-    static boolean[][] visited;
-
-
-    static int result = 0;
+    static boolean flag;
     public static void main(String[] args) throws Exception{
-        input();
-
-        for (int i = 0; i < r; i++) {
-            if (dfs(i, 0)) {
-                result += 1;
-            }
-        }
-
-        System.out.println(result);
-    }
-
-    private static boolean dfs(int thisRow, int thisCol) {
-        if (thisCol == c - 1) {
-            return true;
-        }
-        for (int i = 0; i < 3; i++) {
-            int row = thisRow + dx[i];
-            int col = thisCol + dy[i];
-            if (0 <= row && row < r && 0 <= col && col < c) {
-                if (map[row][col] == '.' && !visited[row][col]) {
-                    visited[row][col] = true;
-                    if (dfs(row, col)) {
-                        map[row][col] = 'x';
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         st = new StringTokenizer(br.readLine());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        map = new char[r][c];
+        bread = new char[r][c];
+        for (int i = 0; i < r; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < c; j++) {
+                bread[i][j] = str.charAt(j);
+            }
+        }
         visited = new boolean[r][c];
         for (int i = 0; i < r; i++) {
-            String thisLine = br.readLine();
-            for (int j = 0; j < c; j++) {
-                map[i][j] = thisLine.charAt(j);
+            flag = true;
+            dfs(i, 0);
+        }
+        System.out.println(result);
+    }
+
+    private static void dfs(int thisRow, int thisCol) {
+        if (thisCol == c - 1) {
+            result += 1;
+            flag = false;
+            return;
+        }
+        for (int k = 0; k < 3 && flag; k++) {
+            int row = thisRow + dx[k];
+            int col = thisCol + dy[k];
+            if (0 <= row && row < r && 0 <= col && col < c) {
+                if (bread[row][col] == '.' && !visited[row][col]) {
+                    visited[row][col] = true;
+                    dfs(row, col);
+                }
             }
         }
     }
-
-
 }
