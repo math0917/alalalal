cee453560dc4d2df4cb9c11ddd78ce9a596362ae
Kim Won Pyo <69501435+math0917@users.noreply.github.com>
2023-02-22 12:14:44 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 6,359 ms, Memory: 20,508 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -1,73 +1,55 @@
-
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-    public static int[] home;
-    public static int[] company;
-    public static int[][] loc;
-    public static int[] numbers;
-    public static int[] combi;
-    public static int n;
-    public static int minValue;
+    static int testCase;
+    static int n;
+    static int startX,startY, finishX, finishY;
+    static int[][] dot;
+    static int min;
     public static void main(String[] args) throws Exception{
-        // TODO Auto-generated method stub
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
-        int testCase = Integer.parseInt(br.readLine());
-
-        for (int t = 1; t < testCase + 1; t++) {
+        testCase = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= testCase; t++) {
+            min = Integer.MAX_VALUE;
             n = Integer.parseInt(br.readLine());
+            dot = new int[n][2];
             st = new StringTokenizer(br.readLine());
-            home = new int[2];
-            company = new int[2];
-            loc = new int[n][2];
-            combi = new int[n];
-            numbers = new int[n];
-            home[0] = Integer.parseInt(st.nextToken());
-            home[1] = Integer.parseInt(st.nextToken());
-
-            company[0] = Integer.parseInt(st.nextToken());
-            company[1] = Integer.parseInt(st.nextToken());
-
+            startX = Integer.parseInt(st.nextToken());
+            startY = Integer.parseInt(st.nextToken());
+            finishX = Integer.parseInt(st.nextToken());
+            finishY = Integer.parseInt(st.nextToken());
             for (int i = 0; i < n; i++) {
-                loc[i][0] = Integer.parseInt(st.nextToken());
-                loc[i][1] = Integer.parseInt(st.nextToken());
-                numbers[i] = i;
+                dot[i][0] = Integer.parseInt(st.nextToken());
+                dot[i][1] = Integer.parseInt(st.nextToken());
             }
-            minValue = Integer.MAX_VALUE;
-            combinations(0,0);
-            System.out.printf("#%d %d\n", t, minValue);
-
+            dfs(0, 0, -1);
+            System.out.println("#"+ t + " " +min);
         }
 
     }
-    static void combinations(int bit, int index) {
-        if (index == n) {
-            
-            int thisTurnResult = 0;
-            for (int i = 0; i < n - 1; i++) {
-                thisTurnResult += Math.abs(loc[combi[i]][0] - loc[combi[i+1]][0]);
-                thisTurnResult += Math.abs(loc[combi[i]][1] - loc[combi[i+1]][1]);
+
+    private static void dfs(int bit, int length, int lastVisit) {
+
+
+        if (bit == 0) {
+            for (int j = 0; j < n; j++) {
+                dfs(1 << j, Math.abs(dot[j][0] - startX) + Math.abs(dot[j][1] - startY), j);
             }
-            thisTurnResult += Math.abs(company[0] - loc[combi[0]][0]);
-            thisTurnResult += Math.abs(company[1] - loc[combi[0]][1]);
-            thisTurnResult += Math.abs(home[0] - loc[combi[n-1]][0]);
-            thisTurnResult += Math.abs(home[1] - loc[combi[n-1]][1]);
-            minValue = Math.min(thisTurnResult, minValue);
+            return;
+        }
+        if (bit == ((int) Math.pow(2, n)) - 1) {
+            min = Math.min(length + Math.abs(dot[lastVisit][0] - finishX) + Math.abs(dot[lastVisit][1] - finishY), min);
         }
         for (int i = 0; i < n; i++) {
-            if (((1<<i) & bit) == 0) {
-                combi[index] = i;
-                combinations(bit | (1<<i), index + 1);
-
+            if ((bit & (1 << i)) ==0) {
+                dfs(bit | (1 << i), length + Math.abs(dot[lastVisit][0] - dot[i][0]) + Math.abs(dot[lastVisit][1] - dot[i][1]), i);
             }
         }
-    }
 
+
+
+    }
 }
