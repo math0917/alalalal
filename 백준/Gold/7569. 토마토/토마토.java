d8b9f3c78e82453771dfd55314df9b899fe379a7
Kim Won Pyo <69501435+math0917@users.noreply.github.com>
2023-03-03 11:47:18 +0900

    [Gold V] Title: 토마토, Time: 592 ms, Memory: 104916 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,75 +1,83 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int[][][] tomato;
-	public static int []dx = new int[] {-1,0,1,0,0,0};
-	public static int []dy = new int[] {0,1,0,-1,0,0};
-	public static int []dz = new int[] {0,0,0,0,1,-1};
-	public static Queue<int[]> queue;
-	public static int result;
-	public static boolean flag = true;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken()),
-				n = Integer.parseInt(st.nextToken()),
-				h = Integer.parseInt(st.nextToken());
-		tomato = new int[h][n][m];
-		queue = new LinkedList<>();
-		for (int k = 0; k < h; k++) {
-			for (int j = 0; j < n; j++) {
-				st = new StringTokenizer(br.readLine());
-				for (int i = 0; i< m;i++) {
-					tomato[k][j][i] = Integer.parseInt(st.nextToken());
-					if (tomato[k][j][i] == 1) {
-						queue.add(new int[] {k,j,i,0});
-					}
-				}
-			}
-		}
-		
-		while (!(queue.isEmpty())){
-			int[] thisTurn = queue.poll();
-			int thisRow = thisTurn[1], thisCol = thisTurn[2], thisHeight = thisTurn[0], thisLength = thisTurn[3];
-			result = thisLength;
-			for (int i = 0; i< 6; i++) {
-				int row = thisRow + dx[i], col = thisCol + dy[i], height = thisHeight + dz[i];
-				
-				if ( 0 <= row && row < n && 0 <= col && col < m && 0 <= height && height < h) {
-						if (tomato[height][row][col] == 0) {
-							tomato[height][row][col] = 1;
-							queue.add(new int[] {height, row, col, thisLength + 1});
-						} 
-							
-						
-				}
-			}
-		}
-		
-		for (int i = 0; i< h; i++) {
-			for (int j = 0; j < n; j++) {
-				for(int k = 0; k < m; k++) {
-					if (tomato[i][j][k] == 0) {	
-						flag = false;
-					}
-				}
-			}
-		}
-		if (flag) {
-			System.out.print(result);
-		} else {
-			System.out.print(-1);
-		}
-		
-		
-		
-		
-		
-	}
+    static int n, m, h;
+    static int[][][] tomato;
+    static int[] dx = {-1, 0, 1, 0, 0, 0};
+    static int[] dy = {0, 1, 0, -1, 0, 0};
+    static int[] dz = {0, 0, 0, 0, 1, -1};
+    static int days = 0;
+    static int ripe = 0;
+    static int cantRipe = 0;
+    static Queue<int[]> queue;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+
+        tomato = new int[m][n][h];
+        queue = new ArrayDeque<>();
+        for (int k = 0; k < h; k++) {
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    tomato[i][j][k] = Integer.parseInt(st.nextToken());
+                    if (tomato[i][j][k] == 1) {
+                        ripe += 1;
+
+                        queue.add(new int[]{i, j, k});
+                    } else if (tomato[i][j][k] == -1) {
+                        cantRipe += 1;
+                    }
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            if (ripe == n * m * h - cantRipe) {
+                System.out.println(days);
+                System.exit(0);
+            }
+            days ++;
+            int daySize = queue.size();
+            int beforeRipe = ripe;
+            for (int p = 0; p < daySize; p++) {
+                int[] info = queue.poll();
+                for (int k = 0; k < dx.length; k++) {
+                    int row = info[0] + dx[k];
+                    int col = info[1] + dy[k];
+                    int height = info[2] + dz[k];
+                    if (canGo(row, col, height)) {
+                        if (tomato[row][col][height] == 0) {
+                            ripe += 1;
+                            tomato[row][col][height] = 1;
+                            queue.add(new int[]{row, col, height});
+                        }
+                    }
+                }
+            }
+
+            if (ripe == beforeRipe && ripe != n * m * h - cantRipe) {
+                System.out.println(-1);
+                System.exit(0);
+            }
+        }
+
+        System.out.println(-1);
+
+
+
+
+    }
+
+    private static boolean canGo(int row, int col, int height) {
+        return 0 <= row && row < m && 0 <= col && col < n && 0 <= height && height < h;
+    }
 }
